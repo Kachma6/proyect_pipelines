9ce3363edd9d3d859b542b7ec1c97a371b58c93a
Kachma6 <karenchoquecallata6@gmail.com>
2024-04-27 21:51:38 -0400

    cambios

--- pipeline_front.groovy
+++ pipeline_front.groovy
@@ -34,13 +34,35 @@ pipeline {
             steps {
                sh "pwd"
                sh "npm version"
-               sh "npm install"
+              sh "npm install"
                sh "npm run build"
+               sh "pwd"
+               sh "tar -rf dist.tar dist/"
+               archiveArtifacts artifacts: 'dist.tar', onlyIfSuccessful=true
+              sh "cp dist.tar /tmp/"
+            }
+        } stage("Test vulnerability"){
+            steps{
+                sh "cd /tmp/"
+                sh "chmod +x /home/grype"
+                sh "pwd"
+                
+                sh "/home/grype /tmp/dist.tar > informe-scan.txt"
+                sh "pwd"
+                archiveArtifacts artifacts: 'informe-scan.txt', onlyIfSuccessful:true
+            }
+        }
+        stage('Build') {
+            steps {
+               sh "pwd"
+               sh "npm version"
+            //    sh "npm install"
+            //    sh "npm run build"
                sh "pwd"
 
 
-                  sh "docker rmi 192.168.137.5:8082/v2/repository/docker/back-prueba:latest | true ; docker build -t 192.168.137.5:8082/v2/repository/docker/back-prueba:latest ."
-                   sh "docker push 192.168.137.5:8082/v2/repository/docker/back-prueba:latest "
+                  sh "docker rmi 192.168.137.5:8082/v2/repository/docker/front-prueba:latest | true ; docker build -t 192.168.137.5:8082/v2/repository/docker/front-prueba:latest ."
+                   sh "docker push 192.168.137.5:8082/v2/repository/docker/front-prueba:latest "
               
 
             }
