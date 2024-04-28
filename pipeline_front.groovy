e04de4b56aaff8a63c95f5e0e708026a3419cb1a
Kachma6 <karenchoquecallata6@gmail.com>
2024-04-27 20:29:28 -0400

    front

---
+++ pipeline_front.groovy
@@ -0,0 +1,42 @@
+def url_repo = "https://github.com/Kachma6/DesafiosEducativos.git"
+
+pipeline {
+    // agent any
+    agent{ 
+        label 'deploy'
+    }
+    
+    parameters{
+        string defaultValue: 'main',description:'colocar el branch del deploy', name: 'BRANCH', trim: false
+    }
+    environment{
+        workspace="/data/"
+       
+    }
+    stages {
+        stage("Limpiar"){
+            steps{
+                cleanWs()
+            }
+        }
+        stage('Download proyect') {
+            steps {
+                // Get some code from a GitHub repository
+                git credentialsId: 'git_credentials', branch : "${BRANCH}", url: "${url_repo}"
+                echo "Proyecto descargado"
+
+            }
+        }
+        stage('Build') {
+            steps {
+               sh "pwd"
+               sh "npm install"
+              
+
+            }
+        }
+       
+      
+        
+    }
+}
